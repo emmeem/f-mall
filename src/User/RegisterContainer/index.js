c4fd2b2e85f17a300297614b50831a49fd0539af
emmeem <1124782639@qq.com>
2020-10-23 11:02:37 +0800

    [ljb] fix format

--- src/User/RegisterContainer/index.js
+++ src/User/RegisterContainer/index.js
@@ -1,59 +1,57 @@
 import React, { Component } from 'react';
-import { Button,Input } from 'antd';
+import { Button, Input } from 'antd';
 
 class RegisterContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-          name:'test',
-          password:'123456',
-          phone:'18117837389',
-          address:'2nd street'
-        }
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'test',
+      password: '123456',
+      phone: '18117837389',
+      address: '2nd street'
+    }
+  }
 
-    register(){
-      const postData ={
-          name: this.state.name,
-          password: this.state.password,
-          phone: this.state.phone,
-          address: this.state.address
-        };
-        console.log(postData);
-        fetch('http://localhost:8080/user/register',{
-                method: "POST",
-                headers:{
-                    "Content-type": "application/json"
-                },
-                body:JSON.stringify(postData)
-            })
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data)
-            })
+  register() {
+    const postData = {
+      name: this.state.name,
+      password: this.state.password,
+      phone: this.state.phone,
+      address: this.state.address
     };
+    fetch('http://localhost:8080/user/register', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  };
 
-    handleUserNameChange = (event)=> {
-      this.setState({
-        name: event.target.value
-      });
-    }
-    handlePassWordChange = (event)=> {
-      this.setState({
-        password: event.target.value
-      });
-    }
-    handlePhoneChange = (event) => {
-      this.setState({
-        phone: event.target.value
-      });
-    }
-    handleAddressChange = (event) => {
-      this.setState({
-        address: event.target.value
-      });
-    }
-  
+  handleUserNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+  handlePassWordChange = (event) => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+  handlePhoneChange = (event) => {
+    this.setState({
+      phone: event.target.value
+    });
+  }
+  handleAddressChange = (event) => {
+    this.setState({
+      address: event.target.value
+    });
+  }
 
   render() {
     return (
@@ -61,23 +59,23 @@ class RegisterContainer extends Component {
         <h1>注册页面</h1>
         <table >
           <tr>
-          <td>用户名:</td>
-          <td><Input placeholder="请输入用户名" style={{width:200}} onChange={this.handleUserNameChange}/></td>
+            <td>用户名:</td>
+            <td><Input placeholder="请输入用户名" style={{ width: 200 }} onChange={this.handleUserNameChange} /></td>
           </tr>
           <tr>
-          <td>密码:</td>
-          <td><Input.Password placeholder="请输入密码" style={{width:200}} onChange={this.handlePassWordChange}/></td>
+            <td>密码:</td>
+            <td><Input.Password placeholder="请输入密码" style={{ width: 200 }} onChange={this.handlePassWordChange} /></td>
           </tr>
           <tr>
-          <td>手机号:</td>
-          <td><Input placeholder="请输入手机号码" style={{width:200}} onChange={this.handlePhoneChange}/></td>
+            <td>手机号:</td>
+            <td><Input placeholder="请输入手机号码" style={{ width: 200 }} onChange={this.handlePhoneChange} /></td>
           </tr>
           <tr>
-          <td>地址:</td>
-          <td><Input placeholder="请输入地址" style={{width:200}} onChange={this.handleAddressChange}/></td>
+            <td>地址:</td>
+            <td><Input placeholder="请输入地址" style={{ width: 200 }} onChange={this.handleAddressChange} /></td>
           </tr>
         </table>
-          <Button type="primary" style={{marginTop:10,width:300}} onClick={()=>(this.register())}>注册</Button>
+        <Button type="primary" style={{ marginTop: 10, width: 300 }} onClick={() => (this.register())}>注册</Button>
       </div>
     );
   }
